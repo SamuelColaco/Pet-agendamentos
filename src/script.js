d363a4811b44df380195daac2115bde1161059ae
Samuel <colaco.samuel8@gmail.com>
2024-09-05 16:13:12 -0300

    Configurando o wbepack e babel

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+"use strict"
+
+import "./styles/global.css"
+import "./styles/main.css"
+import "./styles/modal.css"
+import "./styles/footer.css"
+import "./styles/header.css"
+
+
+import "./libs/dayjs.js"
